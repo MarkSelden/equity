bc210f1c671e989c688e1219772f9f6374e19659
Jack Wallis <jbwallis16@gmail.com>
2019-10-26 13:24:16 -0700

    base functionality scraper

---
+++ scraper.py
@@ -0,0 +1,45 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = 'https://www.ethicalconsumer.org/food-drink/shopping-guide/dairy-free-ice-cream'
+response = requests.get(url)
+print(response)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+#print(soup)
+
+mydivs = soup.findAll("div", {"class": "product-company"})
+
+companies = []
+for div in mydivs:
+    companies.append(div.contents[1].contents[0])
+    
+print(companies)
+
+
+
+
+
+good = soup.findAll("div", {"class": "score good"})
+average = soup.findAll("div", {"class": "score average"})
+bad = soup.findAll("div", {"class": "score bad"})
+
+
+scores = []
+
+for div in good:
+    scores.append(div.contents[0].strip())
+
+for div in average:
+    scores.append(div.contents[0].strip())
+
+for div in bad:
+    scores.append(div.contents[0].strip())
+
+print(scores)
+
+asdf = dict(zip(companies,scores))
+print(asdf.keys())
