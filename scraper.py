cfa233576e602d068e55059d1622d99531924dd0
Jack Wallis <jbwallis16@gmail.com>
2019-10-26 13:28:16 -0700

    scraper V1

--- scraper.py
+++ scraper.py
@@ -3,43 +3,38 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-url = 'https://www.ethicalconsumer.org/food-drink/shopping-guide/dairy-free-ice-cream'
-response = requests.get(url)
-print(response)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+def scraper(url)
+    url = 'https://www.ethicalconsumer.org/food-drink/shopping-guide/dairy-free-ice-cream'
+    response = requests.get(url)
+    print(response)
 
-#print(soup)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-mydivs = soup.findAll("div", {"class": "product-company"})
+    #print(soup)
 
-companies = []
-for div in mydivs:
-    companies.append(div.contents[1].contents[0])
-    
-print(companies)
+    mydivs = soup.findAll("div", {"class": "product-company"})
 
-
+    companies = []
+    for div in mydivs:
+        companies.append(div.contents[1].contents[0])
 
 
+    good = soup.findAll("div", {"class": "score good"})
+    average = soup.findAll("div", {"class": "score average"})
+    bad = soup.findAll("div", {"class": "score bad"})
 
-good = soup.findAll("div", {"class": "score good"})
-average = soup.findAll("div", {"class": "score average"})
-bad = soup.findAll("div", {"class": "score bad"})
 
+    scores = []
 
-scores = []
+    for div in good:
+        scores.append(div.contents[0].strip())
 
-for div in good:
-    scores.append(div.contents[0].strip())
+    for div in average:
+        scores.append(div.contents[0].strip())
 
-for div in average:
-    scores.append(div.contents[0].strip())
+    for div in bad:
+        scores.append(div.contents[0].strip())
 
-for div in bad:
-    scores.append(div.contents[0].strip())
-
-print(scores)
-
-asdf = dict(zip(companies,scores))
-print(asdf.keys())
+    asdf = dict(zip(companies,scores))
+    print(asdf.keys())
