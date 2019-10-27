998062eb9621d95131c44a3edc3eb87d9763effb
Mark Selden <seldenm@berkeley.edu>
2019-10-26 20:46:05 -0700

    Still not done but much closer, pushing every once and awhile just in case

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
-var ratings = require('./result.json');
+//var ratings = require('./result.json');
+const ratings = {"Garnier": 15}
 chrome.storage.local.set({"ratings": ratings});
 
--- popup.js
+++ popup.js
@@ -1,23 +1,59 @@
+alert("popup.js is running");
+var supplier = "not yet found";
+console.log("console log works");
+
+//sets up a promise so that it will assign the values and then afterwards we can call assignments
+const setCompany =  () => {
+	chrome.storage.local.get('company', (result) => {
+		alert("callback is running");
+		supplier = result.company;
+		$('#product').append(supplier); 	
+
+});//end of promise callback function
+};//end of the setComp declaration
+
+const setScore =  () => {
+	chrome.storage.local.get('ratings', (result) => {
+		alert("SetScore running");
+		score = result.ratings[supplier];
+	  	$('#rating').append(score);  	
+
+});//end of promise callback function
+};//end of the setComp declaration
+
+
+
+setCompany();
+setScore();
+
+ 	 
+ 
+
+ 
 
 
 
 
 
-//var companyRatings = chrome.storage.local.get(['ratings'], function(result) {
-	//console.log("success");
-//})
-console.log("4");
-document.addEventListener('DOMContentLoaded', function () {
-	console.log("4");
-	$('#product').append("jif");
-  	$('#rating').append("5");
-  	console.log("5");
-  	alert(chrome.storage.local.get(["company"], function(result) {
-  		console.log("6");
-  		alert(result.key);
-        }));
+/*
+'use strict';
 
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
 });
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+*/
 
 
 
